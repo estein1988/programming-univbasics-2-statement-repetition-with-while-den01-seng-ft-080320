174350973c250b95ef131b87738c7d5405d225dd
Elliott Stein <estein1988@gmail.com>
2020-06-25 21:14:32 +0000

    Automatically backed up by Learn

--- lib/count_down.rb
+++ lib/count_down.rb
@@ -1,12 +1,7 @@
 # Write your code here
 
-count = 10
-
-while count >= 1 do
-  if count <= 10
-    puts count
-  else 
-    puts "Happy New Year!"
-  count -= 1
-  end
+count = 10 
+while count >= 1 do 
+  
+  count -= 1 
 end
